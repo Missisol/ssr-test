7c31644f816b351e4740c66dfc25fe04b4dc9ffb
Vadim Ivanov <102748462+Kitamoru@users.noreply.github.com>
2024-05-02 14:35:06 +0300

    key updated

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
-  <m3sale-widget api-key="1258562c-bc46-4a21-9deb-5e852fc3eba0"></m3sale-widget>
-
-  <NuxtLayout>
+  <m3sale-widget api-key="ef98dcef-52b5-493d-a7cf-a1898c9bf6d5"></m3sale-widget>  <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>  
 </template>
